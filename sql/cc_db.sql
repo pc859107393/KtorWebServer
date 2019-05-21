c2a078bc8bd1e23cc52fabfb838856576661b5bf
pc859107393 <859107393@qq.com>
2019-05-21 23:59:27 +0800

    acheng update sql-script

--- sql/cc_db.sql
+++ sql/cc_db.sql
@@ -10,7 +10,7 @@ Target Server Type    : MYSQL
 Target Server Version : 50716
 File Encoding         : 65001
 
-Date: 2019-05-02 17:43:59
+Date: 2019-05-21 23:58:57
 */
 
 SET FOREIGN_KEY_CHECKS=0;
@@ -25,12 +25,18 @@ CREATE TABLE `admin_menu` (
   `uri` varchar(128) DEFAULT NULL COMMENT '接口地址',
   `icon` varchar(128) DEFAULT NULL COMMENT '接口图标',
   `parent_id` int(11) NOT NULL DEFAULT '0' COMMENT '父菜单默认0，子菜单指定父菜单ID',
+  `sort` int(11) NOT NULL COMMENT '菜单排序',
+  `create_date` datetime DEFAULT NULL COMMENT '创建时间',
+  `update_date` datetime DEFAULT NULL COMMENT '修改时间',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='管理员菜单';
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8 COMMENT='管理员菜单';
 
 -- ----------------------------
 -- Records of admin_menu
 -- ----------------------------
+INSERT INTO `admin_menu` VALUES ('3', '测试父权限', null, 'setting', '0', '1', '2019-05-21 06:55:51', '2019-05-21 06:56:02');
+INSERT INTO `admin_menu` VALUES ('4', '测试子权限', 'www.baidu.com', 'url', '3', '1', '2019-05-21 06:56:05', '2019-05-21 06:56:07');
+INSERT INTO `admin_menu` VALUES ('5', '测试子权限01', 'www.baidu.com', 'url', '3', '2', '2019-05-21 06:56:09', '2019-05-21 06:56:11');
 
 -- ----------------------------
 -- Table structure for admin_user
@@ -41,7 +47,7 @@ CREATE TABLE `admin_user` (
   `name` varchar(50) COLLATE utf8mb4_unicode_ci DEFAULT NULL COMMENT '用户昵称',
   `login_name` varchar(25) COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '-1' COMMENT '用户登录名称',
   `password` varchar(256) COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '-1' COMMENT '用户密码，建议算法：sha256(MD5(明文)+salt)',
-  `duty` varchar(50) COLLATE utf8mb4_unicode_ci DEFAULT NULL COMMENT '用户职位',
+  `duty` varchar(4096) COLLATE utf8mb4_unicode_ci DEFAULT NULL COMMENT '用户职位',
   `create_date` datetime NOT NULL COMMENT '创建时间',
   `used` tinyint(1) NOT NULL DEFAULT '1' COMMENT '账户是否可用',
   PRIMARY KEY (`id`),
@@ -52,7 +58,7 @@ CREATE TABLE `admin_user` (
 -- Records of admin_user
 -- ----------------------------
 INSERT INTO `admin_user` VALUES ('1', '超级管理员', 'pc859107393', '794582130249CA3EEC18A4CFA894022B40108F730EC9413F62A8DF1A4206424B', 'admin', '2018-07-13 03:12:01', '1');
-INSERT INTO `admin_user` VALUES ('2', '管理员1', '228568859', '1b4d2f7c7438009f6bb8154b96e3c75f6a161912fc7ca8b6eb095776499363f7', 'admin', '2018-07-13 03:12:01', '1');
+INSERT INTO `admin_user` VALUES ('2', '管理员1', '228568859', '1b4d2f7c7438009f6bb8154b96e3c75f6a161912fc7ca8b6eb095776499363f7', 'admin', '2018-07-13 03:12:01', '0');
 INSERT INTO `admin_user` VALUES ('3', '管理员999', '12345678', '1b4d2f7c7438009f6bb8154b96e3c75f6a161912fc7ca8b6eb095776499363f7', 'admin', '2018-07-13 03:12:01', '1');
 INSERT INTO `admin_user` VALUES ('4', '管理员9992', 'q23rqw', '1b4d2f7c7438009f6bb8154b96e3c75f6a161912fc7ca8b6eb095776499363f7', 'admin', '2018-07-13 03:12:01', '1');
 INSERT INTO `admin_user` VALUES ('5', 'wwrwer', '1232qewdaf', '1b4d2f7c7438009f6bb8154b96e3c75f6a161912fc7ca8b6eb095776499363f7', 'admin', '2018-07-13 03:12:01', '1');
@@ -84,6 +90,22 @@ INSERT INTO `admin_user` VALUES ('45', 'sfsf', '3terwt', '1b4d2f7c7438009f6bb815
 INSERT INTO `admin_user` VALUES ('46', 'sfsfadzcx', '3terwtqewq', '1b4d2f7c7438009f6bb8154b96e3c75f6a161912fc7ca8b6eb095776499363f7', 'admin', '2018-07-13 03:12:01', '1');
 
 -- ----------------------------
+-- Table structure for site_con
+-- ----------------------------
+DROP TABLE IF EXISTS `site_con`;
+CREATE TABLE `site_con` (
+  `name` varchar(512) NOT NULL,
+  `value` varchar(4096) DEFAULT NULL,
+  PRIMARY KEY (`name`),
+  KEY `site_con_name_index` (`name`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of site_con
+-- ----------------------------
+INSERT INTO `site_con` VALUES ('admin', '[3,4,5]');
+
+-- ----------------------------
 -- Table structure for widgets
 -- ----------------------------
 DROP TABLE IF EXISTS `widgets`;
@@ -100,45 +122,3 @@ CREATE TABLE `widgets` (
 -- ----------------------------
 INSERT INTO `widgets` VALUES ('3', 'asdad砸场子', '564', '2019-03-04 15:03:52');
 INSERT INTO `widgets` VALUES ('4', '刚好风格', '677', '2019-04-04 15:04:03');
-
--- ----------------------------
--- Function structure for getChildLst
--- ----------------------------
-DROP FUNCTION IF EXISTS `getChildLst`;
-DELIMITER ;;
-CREATE DEFINER=`root`@`localhost` FUNCTION `getChildLst`(rootId int) RETURNS varchar(1000) CHARSET utf8
-BEGIN
-        DECLARE sTemp VARCHAR(1000);
-        DECLARE sTempChd VARCHAR(1000);
-
-        SET sTemp = '^';
-        SET sTempChd =cast(rootId as CHAR);
-
-        WHILE sTempChd is not null DO
-            SET sTemp = concat(sTemp,',',sTempChd);
-            SELECT group_concat(id) INTO sTempChd FROM `user` where FIND_IN_SET(agent_id,sTempChd)>0;
-        END WHILE;
-        RETURN sTemp;
-END
-;;
-DELIMITER ;
-
--- ----------------------------
--- Function structure for getcurval
--- ----------------------------
-DROP FUNCTION IF EXISTS `getcurval`;
-DELIMITER ;;
-CREATE DEFINER=`root`@`%` FUNCTION `getcurval`(seq_name varchar(64)) RETURNS int(11)
-BEGIN
-         DECLARE VALUE INTEGER;
-         DECLARE _increment INTEGER;
-         SET VALUE = 10000;
-         SET _increment = 1;
-         SELECT current_value,increment INTO VALUE, _increment
-                   FROM sequence
-                   WHERE NAME = seq_name;
-         REPLACE INTO sequence(NAME,current_value,increment) VALUES (seq_name, VALUE + _increment,_increment);
-         RETURN VALUE;
-         END
-;;
-DELIMITER ;
